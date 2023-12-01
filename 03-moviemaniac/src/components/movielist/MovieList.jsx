d01463eec822bddb8948a2855cfd42322a01fc59
sanju918 <42485083+sanju918@users.noreply.github.com>
2023-12-01 10:51:21 +0530

    added new code

--- 03-moviemaniac/src/components/movielist/MovieList.jsx
+++ 03-moviemaniac/src/components/movielist/MovieList.jsx
@@ -1,16 +1,30 @@
+import { useEffect, useState } from "react";
+import _ from "lodash";
+
 import "./MovieList.css";
 import Fire from "../../assets/fire.png";
 import { MovieCard } from "./MovieCard";
-import { useEffect, useState } from "react";
+import FilterGroup from "./FilterGroup";
 
 export const MovieList = () => {
   const [movies, setMovies] = useState([]);
   const [filterMovies, setFilterMovies] = useState([]);
   const [minRating, setMinRating] = useState(0);
+  const [sort, setSort] = useState({
+    by: "default",
+    order: "asc",
+  });
 
   useEffect(() => {
     fetchMovies();
   }, []);
+
+  useEffect(() => {
+    if (sort.by !== "default") {
+      const sortedMovies = _.orderBy(filterMovies, [sort.by], [sort.order]);
+      setFilterMovies(sortedMovies);
+    }
+  }, [sort]);
 
   const fetchMovies = async () => {
     const res = await fetch(
@@ -35,6 +49,13 @@ export const MovieList = () => {
     }
   };
 
+  const handleSort = (e) => {
+    const { name, value } = e.target;
+    setSort((prev) => ({ ...prev, [name]: value }));
+  };
+
+  // console.log(sort);
+
   return (
     <section className="movie_list">
       {/*  Header */}
@@ -43,46 +64,31 @@ export const MovieList = () => {
           Popular <img src={Fire} alt="fire emoji" className="navbar_emoji" />
         </h2>
         <div className="align_center movie_list_fs">
-          <ul className="align_center movie_filter">
-            <li
-              className={
-                minRating === 8
-                  ? "movie_filter_item active"
-                  : "movie_filter_item"
-              }
-              onClick={() => handleFilter(8)}
-            >
-              8+ Star
-            </li>
-            <li
-              className={
-                minRating === 7
-                  ? "movie_filter_item active"
-                  : "movie_filter_item"
-              }
-              onClick={() => handleFilter(7)}
-            >
-              7+ Star
-            </li>
-            <li
-              className={
-                minRating === 6
-                  ? "movie_filter_item active"
-                  : "movie_filter_item"
-              }
-              onClick={() => handleFilter(6)}
-            >
-              6+ Star
-            </li>
-          </ul>
-          <select name="" id="" className="movie_sorting">
-            <option value="">SortBy</option>
-            <option value="">Date</option>
-            <option value="">Rating</option>
+          <FilterGroup
+            minRating={minRating}
+            onRatingClick={handleFilter}
+            ratings={[8, 7, 6]}
+          />
+          <select
+            name="by"
+            id=""
+            onChange={handleSort}
+            value={sort.by}
+            className="movie_sorting"
+          >
+            <option value="default">SortBy</option>
+            <option value="release_date">Date</option>
+            <option value="vote_average">Rating</option>
           </select>
-          <select name="" id="" className="movie_sorting">
-            <option value="">Ascending</option>
-            <option value="">Descending</option>
+          <select
+            name="order"
+            id=""
+            onChange={handleSort}
+            value={sort.order}
+            className="movie_sorting"
+          >
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
           </select>
         </div>
       </header>
--- 03-moviemaniac/src/components/navbar/Navbar.jsx
+++ 03-moviemaniac/src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import "./Navbar.css";
 import Fire from "../../assets/fire.png";
 import Star from "../../assets/glowing-star.png";
 import Party from "../../assets/partying-face.png";
+import DarkMode from "../DarkMode/DarkMode";
 
 export const Navbar = () => {
   return (
@@ -10,6 +11,7 @@ export const Navbar = () => {
       <nav className="navbar">
         <h1>MovieManiac</h1>
         <div className="navbar_links">
+          <DarkMode />
           <a href="">
             Popular
             <img src={Fire} alt="fire emoji" className="navbar_emoji" />
